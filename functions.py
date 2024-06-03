ab8e9f1572be580cbe4c1a753d0402342b2d5637
ias78 <ias78@tpu.ru>
2024-06-03 15:17:19 +0700

    correct the mistakes

--- functions.py
+++ functions.py
@@ -15,12 +15,12 @@ def mask_card_number(card_number):
     """
     Маскирует номер карты в формате XXXX XX** **** XXXX
     """
-    return f'{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}'
+    return f'{card_number[-16:-12]} {card_number[-11:-9]}** **** {card_number[-4:]}'
 
 
 def mask_account(account_number):
     """
-    Маскирует номер карты в формате XXXX XX** **** XXXX
+    Маскирует номер cчета а формате **XXXX
     """
     return f'**{account_number[-4:]}'
 
@@ -29,10 +29,9 @@ def get_operations(operations):
     """
     Фильтрация и сортировка выполненных операций, и их вывод
     """
-    executed_operations = [op for op in operations if op["state"] == "EXECUTED"]
+    executed_operations = [op for op in operations if op and op["state"] == "EXECUTED"]
     executed_operations.sort(key=lambda x: datetime.fromisoformat(x["date"]), reverse=True)
     last_operations = executed_operations[:5]
     return last_operations
 
 
-print(get_operations(load_operations()))
--- main.py
+++ main.py
@@ -5,26 +5,34 @@ from functions import load_operations, get_operations, mask_account, mask_card_n
 def main():
     for op in get_operations(load_operations()):
         date = datetime.fromisoformat(op["date"]).strftime("%d.%m.%Y")
-        description = op["description"]
-        from_account = op["from"]
-        to_account = op["to"]
+        description = op.get("description", '')
+        from_account = op.get("from", '')
+        to_account = op.get("to", '')
 
         if 'Счет' in from_account:
-            from_account = f"Счет {mask_account(from_account.split()[-1])}"
+            from_account = f"Счет {mask_account(from_account)}"
         else:
-            from_account = mask_card_number(from_account.split()[-1])
+            from_account = mask_card_number(from_account)
 
         if 'Счет' in to_account:
-            to_account = f"Счет {mask_account(to_account.split()[-1])}"
+            to_account = f"Счет {mask_account(to_account)}"
         else:
-            to_account = mask_card_number(to_account.split()[-1])
+            to_account = mask_card_number(to_account)
 
         amount = op['operationAmount']['amount']
         currency = op['operationAmount']['currency']['name']
 
-        return f'''{date}{description}
+        if op["description"] == "Открытие вклада":
+            print(f'''{date} {description}
+-> {to_account}
+{amount} {currency}''')
+        else:
+            print(f'''{date} {description}
 {from_account} -> {to_account}
-{amount} {currency}'''
+{amount} {currency}''')
+
+
+
 
 
 if __name__ == '__main__':
